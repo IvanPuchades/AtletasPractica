8bd097e8cab35be86da4a4998b12ff3c99e69eaa
usu32 <usu32>
2016-12-21 15:56:29 +0100

    Añadiendo Clase y servicio de Atleta

--- src/main/java/RestSynchronous.java
+++ src/main/java/RestSynchronous.java
@@ -1,5 +1,25 @@
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.List;
+
 /**
  * Created by usu32 on 19/12/2016.
  */
 public class RestSynchronous {
+
+    private static Retrofit retrofit;
+
+    public static void main(String[] args) {
+
+        retrofit = new Retrofit.Builder().baseUrl("http://localhost:8080")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        AtletaService atletaService = retrofit.create(AtletaService.class);
+        Call<List<Atleta>> callAllAtleta = atletaService.getAllAtleta();
+
+    }
+
 }
